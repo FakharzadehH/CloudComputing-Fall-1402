0ad73e84291786c38f020175b37b8ca59b3a1472
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2023-10-13 23:02:36 +0330

    feat: added internal and server files

---
+++ Assignments/1/internal/server/server.go
@@ -0,0 +1,13 @@
+package server
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func Start() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	//TODO : Add routes and if middlewares
+	e.Logger.Fatal(e.Start(":1323"))
+}
