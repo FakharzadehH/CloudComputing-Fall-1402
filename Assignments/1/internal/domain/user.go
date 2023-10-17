b39140235ab15ea48b66fe3873ed9d38a553de14
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2023-10-17 13:42:41 +0330

    feat: added user type

---
+++ Assignments/1/internal/domain/user.go
@@ -0,0 +1,12 @@
+package domain
+
+type User struct {
+	UserName   string `json:"user_name"`
+	Email      string `json:"email"`
+	LastName   string `json:"last_name"`
+	NationalID int    `json:"national_id"`
+	IP         string `json:"ip"`
+	State      string `json:"state"`
+	Image1     string `json:"image1"`
+	Image2     string `json:"image2"`
+}
