f35c7ddc71cf7170371ac2f5c98fb1de0aadb926
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2023-10-17 21:02:37 +0330

    feat: added config

---
+++ Assignments/1/internal/config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+var cfg Config
+
+type Config struct {
+	DB DB `mapstructure:"db"`
+}
+
+func GetConfig() Config {
+	return cfg
+}
+func Load(configPath string) error {
+	v := viper.New()
+	v.SetEnvPrefix("CloudComputing")
+	v.SetConfigFile(configPath)
+	v.SetConfigType("yaml")
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	err := v.ReadInConfig()
+	if err != nil {
+		return err
+	}
+
+	err = v.Unmarshal(&cfg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
