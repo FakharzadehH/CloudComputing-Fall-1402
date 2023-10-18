b747c5d0e04343fca2c81e36a305fffcf05f49f0
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2023-10-18 22:10:08 +0330

    feat: changed server start function for better logging

--- Assignments/1/internal/server/server.go
+++ Assignments/1/internal/server/server.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"github.com/FakharzadehH/CloudComputing-Fall-1402/internal/config"
-	"github.com/FakharzadehH/CloudComputing-Fall-1402/internal/logger"
 	"github.com/FakharzadehH/CloudComputing-Fall-1402/internal/repository"
 	"github.com/FakharzadehH/CloudComputing-Fall-1402/internal/server/handlers"
 	"github.com/FakharzadehH/CloudComputing-Fall-1402/service"
@@ -10,18 +9,18 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func Start() {
+func Start() error {
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.HTTPErrorHandler = ErrorHandler()
 	db, err := config.NewGORMConnection(config.GetConfig())
 	if err != nil {
-		logger.Logger().Fatal("err when connecting to db", "err", err)
+		return err
 	}
 	repo := repository.NewRepository(db)
 	svcs := service.New(repo)
 	handler := handlers.New(repo, svcs)
 	routes(e, handler)
 	//TODO : Add routes and if middlewares
-	e.Logger.Fatal(e.Start(":1323"))
+	return e.Start(":1323")
 }
