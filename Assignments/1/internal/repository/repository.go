81a003e09097228ea643387bedb12eed76f37e90
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2023-10-18 22:07:40 +0330

    feat: added repo functions

--- Assignments/1/internal/repository/repository.go
+++ Assignments/1/internal/repository/repository.go
@@ -15,6 +15,15 @@ func NewRepository(db *gorm.DB) *Repository {
 	}
 }
 
-func (r *Repository) GetByNationalID(nationalID int) (domain.User, error) {
-	return domain.User{}, nil
+func (r *Repository) Upsert(user *domain.User) error {
+	return r.db.Save(user).Error
+}
+
+func (r *Repository) GetByID(id int, user *domain.User) error {
+	return r.db.First(user, id).Error
+}
+
+func (r *Repository) GetByNationalID(nationalID string, user *domain.User) error {
+
+	return r.db.First(user, "national_id = ?", nationalID).Error
 }
