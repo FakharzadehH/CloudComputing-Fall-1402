8c0d747d7c044980d2c9554624a40f86d2e7d69d
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2023-10-20 03:24:33 +0330

    feat: added userAuthState

--- Assignments/1/internal/domain/user.go
+++ Assignments/1/internal/domain/user.go
@@ -1,12 +1,20 @@
 package domain
 
 type User struct {
-	ID         int    `json:"id"`
-	Email      string `json:"email"`
-	LastName   string `json:"last_name"`
-	NationalID string `json:"national_id"`
-	IP         string `json:"ip"`
-	State      string `json:"state"`
-	Image1     string `json:"image1"`
-	Image2     string `json:"image2"`
+	ID         int           `json:"id"`
+	Email      string        `json:"email"`
+	LastName   string        `json:"last_name"`
+	NationalID string        `json:"national_id"`
+	IP         string        `json:"ip"`
+	State      UserAuthState `json:"state"`
+	Image1     string        `json:"image1"`
+	Image2     string        `json:"image2"`
 }
+
+type UserAuthState string
+
+const (
+	UserAuthStatePending  UserAuthState = "pending"
+	UserAuthStateDeclined UserAuthState = "declined"
+	UserAuthStateAccepted UserAuthState = "accepted"
+)
