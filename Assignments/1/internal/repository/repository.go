dc0fdf2dbd4644a024979afb1a3b3568239fac85
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2023-10-19 10:57:47 +0330

    feat: added upsert to s3 func

--- Assignments/1/internal/repository/repository.go
+++ Assignments/1/internal/repository/repository.go
@@ -1,7 +1,13 @@
 package repository
 
 import (
+	"mime/multipart"
+
+	"github.com/FakharzadehH/CloudComputing-Fall-1402/internal/config"
 	"github.com/FakharzadehH/CloudComputing-Fall-1402/internal/domain"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"gorm.io/gorm"
 )
 
@@ -26,4 +32,33 @@ func (r *Repository) GetByID(id int, user *domain.User) error {
 func (r *Repository) GetByNationalID(nationalID string, user *domain.User) error {
 
 	return r.db.First(user, "national_id = ?", nationalID).Error
+}
+
+func (r *Repository) UpsertImageIntoS3(img *multipart.FileHeader) error {
+	cfg := config.GetConfig()
+	s3Session, err := session.NewSessionWithOptions(session.Options{
+		Config:  cfg.S3.GenerateS3Config(),
+		Profile: "filebase",
+	})
+	if err != nil {
+		return err
+	}
+	s3Client := s3.New(s3Session)
+	bucket := aws.String(cfg.S3.BucketName)
+	file, err := img.Open()
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	s3Input := &s3.PutObjectInput{
+		Body:   file,
+		Bucket: bucket,
+		Key:    aws.String(img.Filename),
+	}
+	if _, err := s3Client.PutObject(s3Input); err != nil {
+		return err
+	}
+	return nil
+	//check https://docs.filebase.com/code-development-+-sdks/code-development/aws-sdk-go-golang
+
 }
