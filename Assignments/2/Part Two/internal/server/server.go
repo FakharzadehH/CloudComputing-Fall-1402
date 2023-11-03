c6065b662998a6472f50e99f64ef45a00f3cd68c
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2023-11-03 23:34:38 +0330

    feat: added HW2 RESTAPI server Start func

---
+++ Assignments/2/Part Two/internal/server/server.go
@@ -0,0 +1,27 @@
+package server
+
+import (
+	"github.com/FakharzadehH/CloudComputing-Fall-1402/internal/config"
+	"github.com/FakharzadehH/CloudComputing-Fall-1402/internal/repository"
+	"github.com/FakharzadehH/CloudComputing-Fall-1402/internal/server/handlers"
+	"github.com/FakharzadehH/CloudComputing-Fall-1402/service"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func Start() error {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.HTTPErrorHandler = ErrorHandler()
+	rdb, err := config.NewRedisPool(config.GetConfig().Redis)
+	if err != nil {
+		return err
+	}
+	repo := repository.New(rdb)
+	svcs := service.New(repo)
+	h := handlers.New(repo, svcs)
+
+	api := e.Group("/api")
+	api.GET("/check-weather", h.CheckWeather())
+	return e.Start(":1323") // + os.Getenv("SERVER_PORT"))
+}
